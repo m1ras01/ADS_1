e4039b43c48dda28392b4486b4373b571d8e326e
m1ras01 <221930@astanait.edu.kz>
2023-04-09 19:23:28 +0600

    i add 7 th problem

--- src/Main.java
+++ src/Main.java
@@ -2,57 +2,57 @@ import java.util.Scanner;
 public class Main {
     static Scanner s = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Enter the number of problem");
+        System.out.print("Enter the number of problem: ");
         int problem = s.nextInt();
         switch (problem){
             case 1:
-                System.out.println("Lenth of array: ");
+                System.out.print("Lenth of array: ");
                 int n = s.nextInt();
-                System.out.println("Enter the array");
+                System.out.print("Enter the array: ");
                 int[] arr = new int[n];
                 for(int i = 0 ; i < n ; i ++){
                     arr[i] = s.nextInt();
                 }
-                System.out.println("Minimum number is " + Task_1.minNum(arr ,n));
+                System.out.print("Minimum number is: " + Task_1.minNum(arr ,n));
                 break;
             case 2:
-                System.out.println("Enter length of array");
+                System.out.print("Enter length of array: ");
                 n = s.nextInt();
-                System.out.println("Enter the array");
+                System.out.print("Enter the array: ");
                 arr = new int[n];
                 for(int i = 0 ; i <n; i++){
                     arr[i] = s.nextInt();
                 }
-                System.out.println("The average is " + Task_2.averageNums(arr,n));
+                System.out.print("The average is: " + Task_2.averageNums(arr,n));
                 break;
             case 3:
-                System.out.println("Enter a number");
+                System.out.print("Enter a number: ");
                 n = s.nextInt();
-                System.out.println("Your number is "+Task_3.checkNums(n));
+                System.out.print("Your number is: "+Task_3.checkNums(n));
                 break;
             case 4:
-                System.out.println("Enter a number for the find of factorial ");
+                System.out.print("Enter a number for the find of factorial: ");
                 n = s.nextInt();
-                System.out.println("Factorial "+n+"! is " +Task_4.factorial(n));
+                System.out.print("Factorial "+n+"! is: " +Task_4.factorial(n));
                 break;
             case 5:
-                System.out.println("Enter n-th elements in Fibonacci");
+                System.out.print("Enter n-th elements in Fibonacci: ");
                 n = s.nextInt();
-                System.out.println(n+"-th elements in Fibonacci is " +Task_5.FastFibonacci(n));
+                System.out.print(n+"-th elements in Fibonacci is: " +Task_5.FastFibonacci(n));
                 break;
             case 6:
-                System.out.println("Enter a number A");
+                System.out.print("Enter a number A: ");
                 n = s.nextInt();
-                System.out.println("Enter the degree of the number a");
+                System.out.print("Enter the degree of the number a: ");
                 int a = s.nextInt();
-                System.out.println("Degree A of n is "+Task_6.Pow(a,n));
+                System.out.print("Degree A of n is: "+Task_6.Pow(a,n));
                 break;
             case 7:
-                System.out.print("Enter length of array:");
+                System.out.print("Enter length of array: ");
                 n = s.nextInt();
-                arr = new int[n];
-                System.out.print("Enter the array:");
-                System.out.print("Array in reverse order ");
+                System.out.println("Enter the array: ");
+
+                Task_7.rec(n);
 
         }
     }
--- src/Task_7.java
+++ src/Task_7.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 public class Task_7 {
-    private static void rec(int a) {
+    public static void rec(int a) {
         Scanner sc = new Scanner(System.in);
         if(a!=0){
             int num = sc.nextInt();
-            rec(a-1);    // бызде бул типа циклдын ышындегы условия ретынде караймыз сан 0 ге жетпегенше токтамайды
-            System.out.println(num);   // бызде н нын орнын дагы сандары артынан бастап окыгандыктан керысынше жауап ала аламыз
+            rec(a-1);                                                     // бызде бул типа циклдын ышындегы условия ретынде караймыз сан 0 ге жетпегенше токтамайды
+            System.out.println(num);                                         // бызде н нын орнын дагы сандары артынан бастап окыгандыктан керысынше жауап ала аламыз
         }
     }
 }
